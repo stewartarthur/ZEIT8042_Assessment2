cdc4e39ea5ddc4e57a5d0d2201392a0dd1cc93f2
Stewart Arthur <stewart.arthur@gmail.com>
2024-08-24 03:41:08 +1000

    Creating source files and making notes

--- vuln.c
+++ vuln.c
@@ -1,1 +1,4 @@
-#Vulnerable program for Assessment 2
+//Vulnerable program for Assessment 2
+
+//Program to be compiled with one or more security features.
+//exploit.py needs to be created to exploit this program vuln.c
