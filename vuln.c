ce2448030680b80ecf77bf6da1759a304641b2b2
Stewart Arthur <stewart.arthur@gmail.com>
2024-08-25 00:41:26 +1000

    Basic C program

--- vuln.c
+++ vuln.c
@@ -2,3 +2,23 @@
 
 //Program to be compiled with one or more security features.
 //exploit.py needs to be created to exploit this program vuln.c
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+void secret(){
+    printf("You found the secret!\n");
+}
+
+void data_input(){
+    char buf[12];
+    gets(buf);
+    printf("You entered: %s\n", buf);
+}
+
+int main(){
+    data_input();
+    return 0;
+}
