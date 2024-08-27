d741fa062f5fa24e5bdd46adbb19ce671f88b4ee
Stewart Arthur <stewart.arthur@gmail.com>
2024-08-28 09:19:33 +1000

    Lots of testing and learning pwntools

--- vuln.c
+++ vuln.c
@@ -1,42 +1,24 @@
 // Vulnerable program for Assessment 2
 
-// Program to be compiled with one or more security features.
-// exploit.py needs to be created to exploit this program vuln.c
-
-// In the interest of focusing on the exploit and bypassing security
-// features, perhaps remove the 2 x safe inputs and leave as a single bad?
-
-// 260824 - Removed everything but basic vulnerability.
-
-
-// For future refinement/enhancement, consider including less vulnerable parts
-// for more complex program and higher marks?
+// NX Enabled
+// 64bit compiled
 
 #include <stdio.h>
 #include <string.h>
-//#include <stdlib.h>   //needed?
-//#include <unistd.h>   //needed?
 
-// May not base the exploit on finding a hidden function.  Instead, spawn shell?
+//Unused function
 void secret(){
     printf("You found the secret!\n");
 }
 
 void data_input(){
-    //char firstname[64];  //remove?
-    //char lastname[64];   //remove?
-    char age[12];
-    //printf("Enter your first name: ");  //remove?
-    //fgets(firstname, sizeof(firstname), stdin);  //remove?
-    //firstname[strcspn(firstname, "\n")] = '\0';  //remove?
-    //printf("Enter your last name: ");  //remove?
-    //fgets(lastname, sizeof(lastname), stdin);  //remove?
-    //lastname[strcspn(lastname, "\n")] = '\0';
-    printf("Enter your age: ");
-    gets(age);
+    char language[50];
+    printf("buffer:");
+    fflush(stdout);
+    gets(language);
 
     //printf("Thankyou, %s %s; age %s",firstname, lastname, age);  //modify?
-    printf("You entered: %s",age);
+    //printf("You entered: %s",language);
 }
 
 int main(){
